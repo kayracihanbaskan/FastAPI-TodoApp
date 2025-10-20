7d87e0040f351906411cd649c2e7994bb378c777
kayracihanbaskan <cihankayrabaskan@gmail.com>
2025-10-20 14:11:13 +0300

    Pytest File Added Into Folder

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker,declarative_base
+
 
 #SQLALCHEMY_DATABASE_URL = 'sqlite:///./todos.db'
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:yourdatabasepassword@localhost/yourdatabasename'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:Sampa123456@localhost/TodoApplicationDatabase'
 
 #engine = create_engine(SQLALCHEMY_DATABASE_URL,connect_args={'check_same_thread':False})
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from routers import auth, todos
 import models
+from models import Base
 from database import engine
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+@app.get("/healthy/")
+def check_health():
+    return {'status':'Healthy'}
 
 app.include_router(auth.router)
 app.include_router(todos.router)
